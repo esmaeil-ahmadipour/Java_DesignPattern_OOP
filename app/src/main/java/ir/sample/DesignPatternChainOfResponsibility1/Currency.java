f3ee64b59e0a42f455220e1d4c8738e7e0815301
software8686 <software8686@gmail.com>
2019-12-01 18:31:32 +0330

    Commit : DesignPattern / Chain of Responsibility Pattern / Sample 1

---
+++ app/src/main/java/ir/sample/DesignPatternChainOfResponsibility1/Currency.java
@@ -0,0 +1,16 @@
+package ir.sample.DesignPatternChainOfResponsibility1;
+
+public class Currency
+{
+    private int amount ;
+
+    public Currency(int amount)
+    {
+        this.amount = amount;
+    }
+
+    public int getAmount()
+    {
+        return this.amount;
+    }
+}
